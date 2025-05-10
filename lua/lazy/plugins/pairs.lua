9db1b782163ac438a8271a010194130318d5e786
a770 <cap15369@gmail.com>
2025-05-10 20:48:10 +0800

    删除无用的配置和文件

--- lua/lazy/plugins/pairs.lua
+++ lua/lazy/plugins/pairs.lua
@@ -1,14 +1,6 @@
 return {
 	'windwp/nvim-autopairs',
 	config = function()
-		-- 配合cmp,我也不知道有啥效果
-		-- local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-		-- local cmp = require('cmp')
-		-- cmp.event:on(
-		-- 	'confirm_done',
-		-- 	cmp_autopairs.on_confirm_done()
-		-- )
-
 		require('nvim-autopairs').setup({
 			-- 在写markdown时禁用括号补全
 			disable_filetype = {"markdown"},
