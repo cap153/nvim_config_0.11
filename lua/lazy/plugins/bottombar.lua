8e6d327b491cdc0076bde3659489cae9cc5c7d32
tiny <123@qq.com>
2025-09-04 14:06:17 +0000

    格式化

--- lua/lazy/plugins/bottombar.lua
+++ lua/lazy/plugins/bottombar.lua
@@ -6,20 +6,22 @@ return {
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
 	},
-	opts = {
-		options = {
-			theme = "catppuccin",
-			always_divide_middle = false,
-			component_separators = { left = "", right = "" },
-			section_separators = { left = "", right = "" },
-		},
-		sections = {
-			lualine_a = { "mode" },
-			lualine_b = { "branch", "diff", "diagnostics" },
-			lualine_c = { "lsp_status" },
-			lualine_x = {},
-			lualine_y = { "encoding", "progress" },
-			lualine_z = { "location" },
-		},
-	},
+	config = function()
+		require("lualine").setup({
+			options = {
+				theme = "catppuccin",
+				always_divide_middle = false,
+				component_separators = { left = "", right = "" },
+				section_separators = { left = "", right = "" },
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
+				lualine_c = { "lsp_status" },
+				lualine_x = {},
+				lualine_y = { "encoding", "progress" },
+				lualine_z = { "location" },
+			},
+		})
+	end,
 }
